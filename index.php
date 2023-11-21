8e66e2846b8d225c13c922a0816506324700b1fb
FedericaSidoti <federicasidoti21@gmail.com>
2023-11-21 16:14:50 +0100

    Add- other file php

--- index.php
+++ index.php
@@ -3,9 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PhP Badwords</title>
 </head>
 <body>
-    <h1> Hello <h1>
+    <h1> PhP Badwords </h1>
+    <p> Conosco la metà di voi solo a metà e nutro per meno della metà di voi la metà dell'affetto che meritate</p>
+    <form action="badword.php" method="POST">
+        <input type='text' name='bad_word' placeholder='Inserisci la bad word'>
+        <button type='input'>Modifica</button>
+    </form>
 </body>
 </html>
